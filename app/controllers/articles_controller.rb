a302902b2688da82735c2639ca7f65818f12fa28
Zach <zholdren20@gmail.com>
2014-11-05 10:12:38 -0500

    fixed private methods issues

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,24 +1,24 @@
 class ArticlesController < ApplicationController
   def new
   end
-  
+
   def create
     @article = Article.new(article_params)
- 
+
     @article.save
     redirect_to @article
   end
- 
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def index
+    @articles = Article.all
+  end     
+
   private
     def article_params
       params.require(:article).permit(:title, :text)
-    end
-    
-    def show
-      @article = Article.find(params[:id])
-    end
-    
-    def index
-      @articles = Article.all
     end
 end
