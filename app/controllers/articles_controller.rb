d42799db262f2e7a1b48f630f52ac06bc8698f53
Zach <zholdren20@gmail.com>
2014-11-04 19:25:31 -0500

    errors and need some help

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,14 +3,22 @@ class ArticlesController < ApplicationController
   end
   
   def create
-    @article = Article.new(params[:article])
+    @article = Article.new(article_params)
  
     @article.save
     redirect_to @article
   end
-  
+ 
   private
     def article_params
       params.require(:article).permit(:title, :text)
+    end
+    
+    def show
+      @article = Article.find(params[:id])
+    end
+    
+    def index
+      @articles = Article.all
     end
 end
